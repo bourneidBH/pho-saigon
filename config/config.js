b8b30ff5212ef22fce883742fc809e989e946e14
bourne <m.bourne@wi.rr.com>
2020-05-10 08:51:25 -0500

    testing email change

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  USER: process.env.EMAIL, 
-  PASS: process.env.EMAIL_PASSWORD,
+  USER: process.env.EMAIL2, 
+  PASS: process.env.EMAIL2_PASSWORD,
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
   TESTUSER: process.env.TESTUSER,
